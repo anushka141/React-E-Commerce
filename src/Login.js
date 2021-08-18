801159f029d72ef1e9eb24d635dcd5cf70f2fecc
anushka141 <61308296+anushka141@users.noreply.github.com>
2021-08-18 23:36:05 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,35 @@ import "./App.css";
 function Login() {
   return (
     <div className="App">
-      <h1>Login page</h1>
+      <section>
+      <div class="logo" id="login-page-logo">
+        <img src="./public/logo1.png" alt="" />
+      </div>
+      
+      <div class="container" id="login-container">
+        <form action="">
+          <div class="name-tag" id="email-tag">
+            <label for="email">Email : <br /> </label>
+            <input type="email" placeholder="Enter Your Email." />
+          </div>
+          <div class="name-tag" id="password-tag">
+            <label for="password">Password : <br /> </label>
+            <input type="password" placeholder="Enter Your Password" />
+          </div>
+          <div class="button" id="login-button">
+            <button>LOGIN</button>
+          </div>
+        </form>
+      </div>
+
+      <div class="text" id="sign-up-text">
+        <p >New to 3Shoe1 ?</p>
+      </div>
+
+      <div class="button" id="sign-up-button">
+        <button>Sign Up</button>
+      </div>
+    </section>
     </div>
   );
 }
